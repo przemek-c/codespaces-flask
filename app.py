11fb6223b0ffd0622307621ce1096dcbffea4108
przemek-c <105457222+przemek-c@users.noreply.github.com>
2024-04-23 11:24:19 +0000

    damn, it works

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+import subprocess
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    return render_template("index.html", title="Hello")
+
+@app.route('/run_script')
+def run_script():
+    subprocess.call(['python', 'your_script.py'])
     return render_template("index.html", title="Hello")
---
+++ your_script.py
@@ -0,0 +1,30 @@
+import mujoco  # Assuming you have the mujoco library installed
+
+"""
+# Check if installation was succesful.
+try:
+  print('Checking that the installation succeeded:')
+  import mujoco
+  mujoco.MjModel.from_xml_string('<mujoco/>')
+except Exception as e:
+  raise e from RuntimeError(
+      'Something went wrong during installation. Check the shell output above '
+      'for more information.\n'
+      'If using a hosted Colab runtime, make sure you enable GPU acceleration '
+      'by going to the Runtime menu and selecting "Choose runtime type".')
+
+print('Installation successful.')
+"""
+
+xml = """
+<mujoco>
+  <worldbody>
+    <geom name="red_box" type="box" size=".2 .2 .2" rgba="1 0 0 1"/>
+    <geom name="green_sphere" pos=".2 .2 .2" size=".1" rgba="0 1 0 1"/>
+  </worldbody>
+</mujoco>
+"""
+model = mujoco.MjModel.from_xml_string(xml)
+
+# print(model.geom('green_sphere'))
+print('name of geom 1: ', model.geom(1).name)
